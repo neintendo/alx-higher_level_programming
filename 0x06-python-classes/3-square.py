24a2488d0b153a92948dc804686a6255292cc448
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-06-08 17:31:18 +0200

    added 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Creates an empty class Square that defines a square """
+
+
+class Square:
+    """
+    Initialise with size with value checks.
+
+    Arguments:
+        __size: size of the square.
+
+    Return: Nothing.
+    """
+
+    def __init__(self, __size=0):
+        """Initialises the attribute size"""
+        if isinstance(__size, int) != 1:
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = __size
+
+    def area(self):
+        """Returns the current square area."""
+        return (self.__size * self.__size)
