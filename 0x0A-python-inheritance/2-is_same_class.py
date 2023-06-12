73ac9da507a9599cb3ab75ca278c2155233154d1
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-06-12 11:23:04 +0200

    added `2-is_same_class.py` + updated README

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module provides a function that checks if an objects is
+of a specified class.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if the obj is exactly an instance of a_class.
+
+    Arguments:
+        obj: the object to inspect.
+        a_class: the class to check if obj is an instance of.
+
+    Return:
+        True: if obj is an instance of a_class.
+        False: if bj is not an instance of a_class.
+    """
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -8,3 +8,7 @@
 **1-my_list.py:** a class `MyList` that inherits from `list`:
 
 - Public instance method: `def print_sorted(self):` that prints the list, but sorted (ascending sort).
+
+**2-is_same_class.py:**  returns `True` if the object is exactly an instance of the specified class otherwise `False`.
+
+- Prototype: `def is_same_class(obj, a_class):`.
