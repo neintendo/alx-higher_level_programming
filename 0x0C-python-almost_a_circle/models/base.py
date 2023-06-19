c71bac36fcf044e306028dff2c4398d90dd1ca80
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-06-19 03:47:04 +0200

    created a class `Rectangle` + validation of setter and get methods

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module containing a base class for other classes in the project.
+"""
+
+
+class Base:
+    """
+    This is the base class for all the other classes in the project.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialises public instance attribute id"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
